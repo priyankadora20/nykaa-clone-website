f6e90e6f3e26820fdcf6a69c1610f16a8c3d9883
tarun-upadhyay <tarunu88@gmail.com>
2023-02-09 23:16:25 +0530

    schema

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose");
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/")
+const connection = mongoose.connect("mongodb://127.0.0.1:27017/ecom")
 module.exports = { connection}
--- backend/Routes/productRoute.js
+++ backend/Routes/productRoute.js
@@ -1,18 +1,55 @@
-const express = require("express")
-const {ProductModel} = require("../Models/Product.model")
+const express = require("express");
+const { ProductModel } = require("../Models/Product.model");
 
-const productRouter = express.Router()
+const productRouter = express.Router();
 
-productRouter.get("/", async(req, res)=>{
-   const products = await ProductModel.find()
-   res.send(products)
-})
-productRouter.post("/new", async (req, res)=>{
-    console.log(req.body)
-    const product = await ProductModel.create(req.body)
-    res.status(201).json({
+productRouter.get("/", async (req, res) => {
+  const products = await ProductModel.find();
+  res.status(200).json({
+    success: true,
+    products,
+  });
+});
+productRouter.get("/:id", async (req, res)=>{
+    const products = await ProductModel.findById(req.params.id)
+    if(!products) return res.status(500).json({success: false, message: "Product not found"})
+    res.status(200).json({
         success: true,
-        product
+        products
     })
-} )
-module.exports = { productRouter}
+})
+productRouter.post("/new", async (req, res) => {
+  console.log(req.body);
+  const product = await ProductModel.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+});
+
+productRouter.put("/edit/:id", async(req, res)=>{
+    let product = await ProductModel.findById(req.params.id)
+    if(!product){
+        return res.send(500).json({
+            success: false,
+            message: "Product Not found"
+        })
+    }
+    product = await ProductModel.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true, useFindAndModify:false})
+
+    res.status(200).json({
+        success: true,
+        message: "Product updated"
+    })
+})
+
+productRouter.delete("/delete/:id", async(req, res)=>{
+    const product = await ProductModel.findById(req.params.id)
+    if(!product) return res.status(500).json({success:false, message: "Product not Found"})
+    await ProductModel.findByIdAndDelete(req.params.id)
+    res.status(200).json({
+        success: true,
+        message: "Product Deleted Successflly"
+    })
+})
+module.exports = { productRouter };
